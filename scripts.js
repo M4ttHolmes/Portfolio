1918596fd8f896c6251fa01d9f330a79b0c4034f
Matt Holmes <m4ttholmes@gmail.com>
2021-09-27 11:54:24 -0400

    Bootstrap Nav responsive collapse

--- scripts.js
+++ scripts.js
@@ -17,3 +17,9 @@ function loadProjects() {
 function loadContact() {
   page.src = "./contentPages/contact.html";
 }
+
+$(document).ready(function(){
+  $('.navbar-dark > button').on('click', function(){
+      $('.navbar-dark').toggleClass('color-changed');
+  });
+  });
